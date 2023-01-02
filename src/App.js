480b98af2c2bf9751580470ab5f9bd3d00a1ee1d
adityajha0001 <rockadi2407@gmail.com>
2023-01-02 19:02:55 +0530

    folders structure done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Pages from "./component/pages";
+import About from "./component/AboutPage/about";
+import Contact from "./component/ContactPage/contact";
+import Event from "./component/EventsPage/event";
+import Login from "./component/LoginPage/login";
+import Support from "./component/SupportPage/support";
 
 function App() {
   return (
@@ -7,6 +12,11 @@ function App() {
     <BrowserRouter>
         <Routes>
           <Route path="/" element={<Pages/>}/>
+          <Route exact path="/about" element={<About/>}/>
+          <Route exact path="/contact" element={<Contact/>}/>
+          <Route exact path="/event" element={<Event/>}/>
+          <Route exact path="/login" element={<Login/>}/>
+          <Route exact path="/support" element={<Support/>}/>
         </Routes>
     </BrowserRouter>
     </>
--- src/component/LandingPage/nav.js
+++ src/component/LandingPage/nav.js
@@ -1,7 +1,7 @@
 export default function Nav(){
     return(
         <>
-        
+        <h1 className="flex justify-center text-8xl font-bold py-64">Welcome Team</h1>
         </>
     )
 };
